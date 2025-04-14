901afff22afa6085fb524a05b0ab782a8172927f
ssarkar90 <susanta.sarkar@dedalus.com>
2025-04-14 12:24:17 +0530

    HM-5 done development

--- src/main/java/org/dedalus/GreetingResource.java
+++ src/main/java/org/dedalus/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        System.out.println("Hello this is testing More testing for Merging ");
+        System.out.println("Hello this is testing More testing for Merging and transit ");
         return "Hello from Quarkus REST";
     }
 }
